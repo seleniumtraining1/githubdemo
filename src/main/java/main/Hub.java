0c4773a744fd6e6cdeb8e33a16683b1ca1bf2b38
Michael PalotasVM <michael.palotas@gridfusion.net>
2013-06-25 11:38:26 -0700

    add new file

---
+++ src/main/java/main/Hub.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Hub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
