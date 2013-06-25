5286ac22de2ea05d2186784c7af983ce3f0adfe7
Michael PalotasVM <michael.palotas@gridfusion.net>
2013-06-25 14:00:36 -0700

    add unit test directory

---
+++ src/test/java/Tests/CalculatorTest.java
@@ -0,0 +1,22 @@
+package Tests;
+
+import junit.framework.Assert;
+import main.Calculator;
+
+import org.testng.annotations.Test;
+
+public class CalculatorTest {
+	
+	@Test
+	public void addTest1() {
+		Calculator calc=new Calculator();
+		Assert.assertEquals(3, calc.add(1, 2));
+	}
+
+	
+	@Test
+	public void addTest2() {
+		Calculator calc=new Calculator();
+		Assert.assertEquals(0, calc.add(0, 0));
+	}
+}
